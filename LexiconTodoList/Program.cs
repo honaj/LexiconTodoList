612ef36affccaf3fc3e59e915a2c2052e095d6f0
Johan Hellgren <honajnahoj@gmail.com>
2023-07-17 10:27:10 +0200

    Initial commit

---
+++ LexiconTodoList/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Welcome to your todo-list!");
+Console.WriteLine("You x unfinished tasks and y tasks are done!");
+
+List<TodoItem> pendingItems = new();
+List<TodoItem> finishedItems = new();
+
+MainLoop();
+return;
+
+void MainLoop()
+{
+    
+}
+
+async Task LoadTodoItems()
+{
+    try
+    {
+        var jsonPath = "TodoData.json";
+    }
+    catch
+    {
+        
+    }
+}
+
+void PrintPendingItems()
+{
+    Console.WriteLine("rtest");
+}
+
+public class TodoItem
+{
+    public bool Done;
+    public string Name;
+    public DateTime DueDate;
+    public string Project;
+
+    public TodoItem(bool done, string name, DateTime dueDate, string project)
+    {
+        Done = done;
+        Name = name;
+        DueDate = dueDate;
+        Project = project;
+    }
+}
